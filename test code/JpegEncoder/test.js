2a1c14e7412aa6019352e5c403c46c0976674571
quocthai200x <quocthai2000x@gmail.com>
2021-06-12 22:26:48 +0700

    modify to personal code

--- test code/JpegEncoder/test.js
+++ test code/JpegEncoder/test.js
@@ -34,7 +34,7 @@ function JPEGEncoder(quality) {
     var clt = new Array(256);
  
     var currentQuality;
-    var know = 3;
+  
     var ZigZag = [
         0, 1, 5, 6, 14, 15, 27, 28,
         2, 4, 7, 13, 16, 26, 29, 42,
